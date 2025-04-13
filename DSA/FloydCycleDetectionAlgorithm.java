f674517aec156032edb4f6bd036c717fa7e13ab6
madhusudan kumar <madhusudanshambhu@gmail.com>
2025-04-13 15:56:31 +0530

    implemented code to get entry point

--- DSA/FloydCycleDetectionAlgorithm.java
+++ DSA/FloydCycleDetectionAlgorithm.java
@@ -2,12 +2,12 @@ package java_collection.DSA;
 
 
 /*
-*
-* Floyd’s Cycle Detection Algorithm is a classic example of the two pointers method.
-*
-* O(n) — You only go through the list once, and inside the cycle, it takes at most k steps (size of cycle) to detect it.
-*
-* */
+ *
+ * Floyd’s Cycle Detection Algorithm is a classic example of the two pointers method.
+ *
+ * O(n) — You only go through the list once, and inside the cycle, it takes at most k steps (size of cycle) to detect it.
+ *
+ * */
 
 public class FloydCycleDetectionAlgorithm {
     public static void main(String[] args) {
@@ -18,8 +18,29 @@ public class FloydCycleDetectionAlgorithm {
         head.next.next.next.next = head.next;
 
         Boolean detectCycle = detectCycle(head);
-        System.out.println(detectCycle);
+        System.out.println("Cycle detected = " + detectCycle);
+        int detectCycleStart = detectCycleStart(head);
+        System.out.println("cycle entry is at node = "+detectCycleStart);
 
+    }
+
+    private static int detectCycleStart(Node head) {
+        Node slow = head;
+        Node fast = head;
+        Node entry = null;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                entry=head;
+                while(entry!=slow){
+                    entry=entry.next;
+                    slow=slow.next;
+                }
+                return entry.data;
+            }
+        }
+        return -1;
     }
 
     private static Boolean detectCycle(Node node) {
